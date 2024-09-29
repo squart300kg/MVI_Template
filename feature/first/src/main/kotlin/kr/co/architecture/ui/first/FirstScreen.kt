f41640f97e0dc5d3097a5f626a21a751c4447e8f
sysong <sysong@pentasecurity.com>
2024-09-29 18:03:48 +0900

    padding적용

--- feature/first/src/main/kotlin/kr/co/architecture/ui/first/FirstScreen.kt
+++ feature/first/src/main/kotlin/kr/co/architecture/ui/first/FirstScreen.kt
@@ -1,14 +1,20 @@
 package kr.co.architecture.ui.first
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -58,6 +64,14 @@ fun FirstScreen(
             LazyColumn(modifier) {
                 items(uiState.uiModels) { item ->
                     Text(
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .border(
+                                width = 1.dp,
+                                shape = RoundedCornerShape(4.dp),
+                                color = Color.LightGray
+                            )
+                            .padding(8.dp),
                         text = item.name,
                         style = TextStyle(
                             fontSize = 20.sp,
--- feature/second/src/main/kotlin/kr/co/architecture/ui/second/SecondScreen.kt
+++ feature/second/src/main/kotlin/kr/co/architecture/ui/second/SecondScreen.kt
@@ -1,13 +1,18 @@
 package kr.co.architecture.ui.second
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -54,6 +59,14 @@ fun SecondScreen(
             LazyColumn(modifier) {
                 items(uiState.uiModels) { item ->
                     Text(
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .border(
+                                width = 1.dp,
+                                shape = RoundedCornerShape(4.dp),
+                                color = Color.LightGray
+                            )
+                            .padding(8.dp),
                         text = item.name,
                         style = TextStyle(
                             fontSize = 20.sp,
