f664272fbe04e875141bcbed1d014ca390f98d3f
sysong <sysong@pentasecurity.com>
2024-11-06 13:28:39 +0900

    ResponseBaseOperators내부 수정

--- core/network/src/main/kotlin/kr/co/architecture/core/network/operator/ResponseBaseOperator.kt
+++ core/network/src/main/kotlin/kr/co/architecture/core/network/operator/ResponseBaseOperator.kt
@@ -2,6 +2,7 @@ package kr.co.architecture.core.network.operator
 
 import com.skydoves.sandwich.ApiResponse
 import com.skydoves.sandwich.operators.ApiResponseSuspendOperator
+import com.skydoves.sandwich.retrofit.raw
 import kr.co.architecture.core.model.ArchitectureSampleHttpException
 import kr.co.architecture.core.network.model.CommonResponse
 
@@ -21,14 +22,14 @@ class ResponseBaseOperator<ENTITY, DTO>(
         }
     }
 
-    override suspend fun onError(apiResponse: ApiResponse.Failure.Error<CommonResponse<ENTITY>>) {
+    override suspend fun onError(apiResponse: ApiResponse.Failure.Error) {
         throw ArchitectureSampleHttpException(
             code = apiResponse.raw.code,
             message = apiResponse.raw.message,
         )
     }
 
-    override suspend fun onException(apiResponse: ApiResponse.Failure.Exception<CommonResponse<ENTITY>>) {
-        throw apiResponse.exception
+    override suspend fun onException(apiResponse: ApiResponse.Failure.Exception) {
+        throw apiResponse.throwable
     }
 }
