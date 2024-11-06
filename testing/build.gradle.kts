754d576530510f305d82aad2309bec93f9e6a943
sysong <sysong@pentasecurity.com>
2024-11-06 13:55:20 +0900

    testing프로젝트 build.gradle.kts 정리

--- testing/build.gradle.kts
+++ testing/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.architecture.sample.base.setting)
-    alias(libs.plugins.jetbrains.kotlin)
 }
 
 android {
@@ -13,7 +12,6 @@ android {
         implementation(project(":core:model"))
         implementation(project(":core:common"))
         implementation(project(":core:repository"))
-        implementation(project(":core:network"))
 
         implementation(libs.androidx.test.rules)
         implementation(libs.kotlinx.coroutines.test)
