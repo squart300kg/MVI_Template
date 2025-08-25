85e9b946c4f51f97e0c6b7ad471e9cc0cbb6cfd5
sysong <sysong@pentasecurity.com>
2025-08-25 14:52:43 +0900

    chore: core:router 모듈 추가

---
+++ core/router/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+  alias(libs.plugins.android.library)
+  alias(libs.plugins.architecture.sample.base.setting)
+  alias(libs.plugins.architecture.sample.ui)
+}
+
+android {
+  namespace = "kr.co.architecture.core.router"
+
+  defaultConfig { }
+
+  dependencies {
+    implementation(project(":core:model"))
+  }
+}
