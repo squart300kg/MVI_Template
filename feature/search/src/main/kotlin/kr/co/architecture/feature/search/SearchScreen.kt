9825ff2c4ac1ac6f034abe3e56f1112a032c65db
sysong <sysong@pentasecurity.com>
2025-08-28 14:00:12 +0900

    feat: 키패드 내림

--- feature/search/src/main/kotlin/kr/co/architecture/feature/search/SearchScreen.kt
+++ feature/search/src/main/kotlin/kr/co/architecture/feature/search/SearchScreen.kt
@@ -44,6 +44,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -259,8 +261,10 @@ fun SearchHeader(
   onSearch: () -> Unit,
   onChangeSort: (SortTypeEnum) -> Unit
 ) {
+  val focusManager = LocalFocusManager.current
+  val keyboardController = LocalSoftwareKeyboardController.current
+
   Column(modifier = modifier.padding(horizontal = 16.dp, vertical = 8.dp)) {
-    // 검색창
     OutlinedTextField(
       modifier = Modifier
         .fillMaxWidth()
@@ -279,7 +283,13 @@ fun SearchHeader(
       singleLine = true,
       shape = RoundedCornerShape(24.dp),
       keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
-      keyboardActions = KeyboardActions(onSearch = { onSearch() })
+      keyboardActions = KeyboardActions(
+        onSearch = {
+          onSearch()
+          focusManager.clearFocus(force = true)
+          keyboardController?.hide()
+        }
+      )
     )
 
     Spacer(Modifier.height(12.dp))
