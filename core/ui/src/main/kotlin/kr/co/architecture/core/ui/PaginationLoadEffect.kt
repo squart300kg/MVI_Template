fee7235a1db1b922d4c0370ec216a1fe6923a4c3
sysong <sysong@pentasecurity.com>
2025-08-28 09:54:57 +0900

    feat: PaginationLoadEffect 추가

---
+++ core/ui/src/main/kotlin/kr/co/architecture/core/ui/PaginationLoadEffect.kt
@@ -0,0 +1,29 @@
+package kr.co.architecture.core.ui
+
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+
+@Composable
+fun PaginationLoadEffect(
+  listState: LazyListState,
+  onScrollToEnd: () -> Unit,
+  bufferItemCount: Int = 5
+) {
+  val shouldLoadMore by remember {
+    derivedStateOf {
+      val layoutInfo = listState.layoutInfo
+      val lastVisibleItemIndex = layoutInfo.visibleItemsInfo.lastOrNull()?.index ?: return@derivedStateOf false
+      val totalItemCount = layoutInfo.totalItemsCount
+
+      totalItemCount != 0 &&
+        lastVisibleItemIndex >= totalItemCount - bufferItemCount
+    }
+  }
+  LaunchedEffect(shouldLoadMore) {
+    if (shouldLoadMore) onScrollToEnd()
+  }
+}
