a109934fff130ece1ed7af8d7638e4610415b5df
sysong <sysong@pentasecurity.com>
2025-09-02 10:24:28 +0900

    refactor: UiText Immutable하게 설정

--- core/ui/src/main/kotlin/kr/co/architecture/core/ui/util/UiText.kt
+++ core/ui/src/main/kotlin/kr/co/architecture/core/ui/util/UiText.kt
@@ -3,22 +3,30 @@ package kr.co.architecture.core.ui.util
 import android.content.Context
 import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
 import androidx.compose.ui.res.stringResource
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
+import kotlinx.collections.immutable.toImmutableList
 
+@Immutable
 data class UiText(
   val value: String? = null,
   @StringRes val resId: Int? = null,
-  val args: List<Any> = emptyList(),
-  val parts: List<UiText> = emptyList()
+  val args: ImmutableList<Any> = persistentListOf(),
+  val parts: ImmutableList<UiText> = persistentListOf()
 ) {
   companion object {
     fun DynamicString(value: String) = UiText(value = value)
     fun StringResource(@StringRes resId: Int, args : List<Any> = emptyList()) =
-      UiText(resId = resId, args = args)
-    fun Combined(parts: List<UiText>) = UiText(parts = parts)
+      UiText(resId = resId, args = args.toImmutableList())
+    fun Combined(parts: List<UiText>) = UiText(parts = parts.toImmutableList())
   }
 }
 
+/**
+ * `plus`함수는 UiText.StringResource와 UiText.DynamicString를 '+' 연산자를 통해 합치기 기능을 제공
+ */
 operator fun UiText.plus(other: UiText): UiText =
   UiText.Combined(listOf(this, other))
 
