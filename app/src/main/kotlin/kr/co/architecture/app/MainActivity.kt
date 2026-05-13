05caa13418ced70726eabffd4ee9ecd4d1718334
songsang-yoon <a01039329810@gmail.com>
2026-05-13 12:39:00 +0900

    엣지 투 엣지 인셋 처리를 정리

--- app/src/main/kotlin/kr/co/architecture/app/MainActivity.kt
+++ app/src/main/kotlin/kr/co/architecture/app/MainActivity.kt
@@ -1,10 +1,15 @@
 package kr.co.architecture.app
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -30,6 +35,17 @@ class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
+    enableEdgeToEdge(
+      statusBarStyle = SystemBarStyle.auto(
+        lightScrim = Color.TRANSPARENT,
+        darkScrim = Color.TRANSPARENT
+      ),
+      navigationBarStyle = SystemBarStyle.auto(
+        lightScrim = Color.TRANSPARENT,
+        darkScrim = Color.TRANSPARENT
+      )
+    )
+
     setContent {
       val loadingState by viewModel.globalUiBus.loadingState.collectAsStateWithLifecycle()
       val errorMessageState by viewModel.globalUiBus.failureDialog.collectAsStateWithLifecycle()
@@ -39,6 +55,7 @@ class MainActivity : ComponentActivity() {
 
       BaseTheme {
         Scaffold(
+          contentWindowInsets = WindowInsets.safeDrawing,
           bottomBar = {
             BaseNavigationBarWithItems(
               currentTab = navigator.currentTab,
@@ -48,7 +65,7 @@ class MainActivity : ComponentActivity() {
               }
             )
           },
-          content =  { innerPadding ->
+          content = { innerPadding ->
             NavHost(
               modifier = Modifier.padding(innerPadding),
               navController = navigator.navController,
